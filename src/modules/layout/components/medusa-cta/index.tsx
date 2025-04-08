bf9288b67a8e3d684a1a2da81c7327987887207c
Tim Hrdy <tim.hrdy.1@gmail.com>
2025-04-08 16:07:09 +0200

    zob

--- src/modules/layout/components/medusa-cta/index.tsx
+++ src/modules/layout/components/medusa-cta/index.tsx
@@ -1,11 +1,12 @@
 import { Text } from "@medusajs/ui"
-import { FaFacebook, FaInstagram, FaTripadvisor } from "react-icons/fa"
+import { FaFacebook, FaInstagram } from "react-icons/fa"
+import { FaTiktok } from "react-icons/fa6"
 
 const MedusaCTA = () => {
   return (
     <Text className="flex gap-x-4 txt-compact-small-plus items-center">
       <a
-        href="https://facebook.com"
+        href="https://www.facebook.com/people/La-Bella-Vista/61558146489458/"
         target="_blank"
         rel="noreferrer"
         className="text-gray-500 hover:text-black transition-colors"
@@ -14,7 +15,7 @@ const MedusaCTA = () => {
         <FaFacebook size={20} />
       </a>
       <a
-        href="https://instagram.com"
+        href="https://www.instagram.com/labellavista91250/"
         target="_blank"
         rel="noreferrer"
         className="text-gray-500 hover:text-black transition-colors"
@@ -23,13 +24,13 @@ const MedusaCTA = () => {
         <FaInstagram size={20} />
       </a>
       <a
-        href="https://tripadvisor.fr"
+        href="https://www.tiktok.com/@la.bellavista"
         target="_blank"
         rel="noreferrer"
         className="text-gray-500 hover:text-black transition-colors"
-        aria-label="TripAdvisor"
+        aria-label="TikTok"
       >
-        <FaTripadvisor size={20} />
+        <FaTiktok size={20} />
       </a>
     </Text>
   )
